87bd88c4ac1b55adfdf2c5165c1cf74eb69f811c
Karau Lutz <lutz.karau@ers-hameln.de>
2023-03-13 13:00:51 +0100

    feat: new world for third person demo created

---
+++ KWEngine3TestProject/Classes/WorldThirdPersonView/PlayerThirdPerson.cs
@@ -0,0 +1,66 @@
+﻿using KWEngine3;
+using KWEngine3.GameObjects;
+using KWEngine3.Helper;
+using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+
+namespace KWEngine3TestProject.Classes.WorldThirdPersonView
+{
+    public class PlayerThirdPerson : GameObject
+    {
+        private float _rotationScaleFactor = 1f;
+        private Vector2 _currentCameraRotation = new Vector2(0, 0);
+        private float _limitYUp = 5;
+        private float _limitYDown = -75;
+
+        public override void Act()
+        {
+            // Wenn sich die Spielfigur mitdrehen soll, muss hier
+            // die Mausbewegung auch zur Player-Rotation addiert werden!
+            this.AddRotationY(-MouseMovement.X * KWEngine.MouseSensitivity);
+
+            // Diese Methode berechnet die neue Kameraposition:
+            UpdateCameraPosition(MouseMovement * KWEngine.MouseSensitivity);
+        }
+
+        private void UpdateCameraPosition(Vector2 msMovement)
+        {
+            // Berechne anhand der Mausbewegung, um wieviel Grad die Kamera
+            // sich drehen müsste:
+            _currentCameraRotation.X += msMovement.X * _rotationScaleFactor;
+            _currentCameraRotation.Y += msMovement.Y * _rotationScaleFactor;
+            // Damit die Kamera nicht "über Kopf" geht, wird die Rotation nach
+            // oben und unten begrenzt:
+            if (_currentCameraRotation.Y < _limitYDown)
+            {
+                _currentCameraRotation.Y = _limitYDown;
+            }
+            if (_currentCameraRotation.Y > _limitYUp)
+            {
+                _currentCameraRotation.Y = _limitYUp;
+            }
+            // Erfrage aktuelle Blickrichtung und Position der Spielfigur:
+            Vector3 lookAtVector = LookAtVector;
+            Vector3 playerPosition = Center;
+            // Berechne für Kameraposition und -ziel einen individuellen Offset-Wert:
+            float lav_factor = (0.00012f * (_currentCameraRotation.Y * _currentCameraRotation.Y) + 0.02099f * _currentCameraRotation.Y + 0.89190f);
+            float lav_factor2 = _currentCameraRotation.Y >= -15 ? (_currentCameraRotation.Y + 15) / 20f : 0f;
+            Vector3 offsetCamPos = HelperRotation.RotateVector(lookAtVector, -90, Plane.XZ) + lookAtVector * 5 * lav_factor;
+            Vector3 offsetCamTarget = HelperRotation.RotateVector(lookAtVector, -90, Plane.XZ) + lookAtVector * 2 + Vector3.UnitY * 2 * lav_factor2;
+
+            // Berechne die neue Kameraposition anhand der gesammelten Infos:
+            Vector3 newCamPos = HelperRotation.CalculateRotationForArcBallCamera(
+                    playerPosition,                  // Drehpunkt
+                    10f,                             // Distanz zum Drehpunkt
+                    _currentCameraRotation.X,        // Drehung links/rechts
+                    _currentCameraRotation.Y,        // Drehung oben/unten
+                    false,                           // invertiere links/rechts?
+                    false                            // invertiere oben/unten?
+            );
+            // Setze die neue Kameraposition und das Kameraziel:
+            CurrentWorld.SetCameraPosition(newCamPos + offsetCamPos);
+            CurrentWorld.SetCameraTarget(playerPosition + offsetCamTarget);
+        }
+    }
+}
---
+++ KWEngine3TestProject/Worlds/GameWorldThirdPersonView.cs
@@ -0,0 +1,44 @@
+﻿using KWEngine3;
+using KWEngine3TestProject.Classes;
+using KWEngine3TestProject.Classes.WorldThirdPersonView;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWEngine3TestProject.Worlds
+{
+    public class GameWorldThirdPersonView : World
+    {
+        public override void Act()
+        {
+            
+        }
+
+        public override void Prepare()
+        {
+            Immovable f01 = new Immovable();
+            f01.Name = "Floor";
+            f01.SetModel("KWCube");
+            f01.SetTexture(@"./textures/tiles_albedo.jpg", TextureType.Albedo);       // regular texture file
+            f01.SetTexture(@"./textures/tiles_normal.jpg", TextureType.Normal);       // (optional) normal map for custom light reflections
+            f01.SetTexture(@"./textures/tiles_roughness.png", TextureType.Roughness); // (optional) roughness map for specular highlights
+            f01.SetTextureRepeat(3, 3);                                               // how many times the texture is tiled across the object?
+            f01.SetScale(15, 0.2f, 15);
+            f01.SetPosition(0, -0.1f, 0);
+            f01.IsShadowCaster = true;                                                // does the object cast and receive shadows? (default: false)
+            AddGameObject(f01);
+
+            PlayerThirdPerson p01 = new PlayerThirdPerson();
+            p01.SetModel("KWCube");
+            p01.SetPosition(0, 1, 0);
+            p01.SetScale(1, 2, 1);
+            p01.IsShadowCaster = true;
+            p01.SetRotation(0, 180, 0);
+            AddGameObject(p01);
+
+            MouseCursorGrab();
+        }
+    }
+}
