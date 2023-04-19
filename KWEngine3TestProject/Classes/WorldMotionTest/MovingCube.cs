a8fa97f33b493091a518fee786aacbad368959cb
Lutz Karau <Karau_L@ers-hm.de>
2023-04-19 07:36:52 +0200

    added motion test world for testing smooth rendering

---
+++ KWEngine3TestProject/Classes/WorldMotionTest/MovingCube.cs
@@ -0,0 +1,25 @@
+﻿using KWEngine3.GameObjects;
+using KWEngine3.Helper;
+using OpenTK.Mathematics;
+
+namespace KWEngine3TestProject.Classes.WorldMotionTest
+{
+    public class MovingCube : GameObject
+    {
+        private Vector3 _pivot;
+        private float _degrees;
+
+        public MovingCube(Vector3 pivot, float degrees)
+        {
+            _pivot = pivot;
+            _degrees = degrees;
+        }
+
+        public override void Act()
+        {
+            _degrees++;
+            Vector3 newPos = HelperRotation.CalculatePositionAfterRotationAroundPointOnAxis(_pivot, 2, _degrees);
+            SetPosition(newPos);
+        }
+    }
+}
---
+++ KWEngine3TestProject/Worlds/GameWorldMotionTest.cs
@@ -0,0 +1,43 @@
+﻿using KWEngine3;
+using KWEngine3.Helper;
+using KWEngine3TestProject.Classes;
+using KWEngine3TestProject.Classes.WorldMotionTest;
+using OpenTK.Mathematics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWEngine3TestProject.Worlds
+{
+    internal class GameWorldMotionTest : World
+    {
+        public override void Act()
+        {
+            
+        }
+
+        public override void Prepare()
+        {
+            SetCameraPosition(0, 10, 50);
+
+            for(int i = 0; i < 100; i++)
+            {
+                MovingCube mc = new MovingCube(
+                    new Vector3(
+                        HelperRandom.GetRandomNumber(-25f, 25f),
+                        HelperRandom.GetRandomNumber(-25f, 25f),
+                        HelperRandom.GetRandomNumber(-25f, 25f)
+                    ), 
+                    HelperRandom.GetRandomNumber(0f, 359f));
+                mc.SetColor(
+                        HelperRandom.GetRandomNumber(0.3f, 1f),
+                        HelperRandom.GetRandomNumber(0.3f, 1f),
+                        HelperRandom.GetRandomNumber(0.3f, 1f)
+                    );
+                AddGameObject(mc);
+            }
+        }
+    }
+}
