a2b9b99ba9e278bfbe33f8358fb159cbe110c762
Lutz Karau <lutz.karau@ers-hameln.de>
2023-11-26 09:47:15 +0100

    changed start world

--- KWEngine3TestProject/GameWindow.cs
+++ KWEngine3TestProject/GameWindow.cs
@@ -7,7 +7,7 @@ namespace KWEngine3TestProject
     {
         public GameWindow() : base(1280, 720)
         {
-            SetWorld(new GameWorldStart());
+            SetWorld(new GameWorldCollisionTest());
         }
     }
 }
