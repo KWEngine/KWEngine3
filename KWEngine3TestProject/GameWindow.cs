6f64c1098cab4f9bcfce06cf1902d4849236c12a
Lutz Karau @ ERS <lutz.karau@ers-hameln.de>
2024-01-30 08:06:03 +0100

    created branch for testing purposes

--- KWEngine3TestProject/GameWindow.cs
+++ KWEngine3TestProject/GameWindow.cs
@@ -5,9 +5,9 @@ namespace KWEngine3TestProject
 {
     public class GameWindow : GLWindow
     {
-        public GameWindow() : base(1280, 720)
+        public GameWindow() : base(1280, 720, false)
         {
-            SetWorld(new GameWorldStart());
+            SetWorld(new GameWorldGLTFNodesTest());
         }
     }
 }
---
+++ KWEngine3TestProject/Worlds/GameWorldGLTFNodesTest.cs
@@ -0,0 +1,44 @@
+﻿using KWEngine3;
+using KWEngine3.GameObjects;
+using KWEngine3TestProject.Classes.WorldGLTFNodesTest;
+using System;
+using System.Collections.Generic;
+
+
+namespace KWEngine3TestProject.Worlds
+{
+    internal class GameWorldGLTFNodesTest : World
+    {
+        public override void Act()
+        {
+            
+        }
+
+        public override void Prepare()
+        {
+            KWEngine.LoadModel("Grass1", "./Models/GLTFTest/grass/grass1.gltf");
+            SetCameraPosition(10, 10, 10);
+            SetColorAmbient(0.25f, 0.25f, 0.25f);
+
+            Grass g = new Grass();
+            g.SetModel("Grass1");
+            g.IsShadowCaster = true;
+            g.HasTransparencyTexture = true;
+            g.DisableBackfaceCulling = true;
+            AddRenderObject(g);
+
+            Floor f = new Floor();
+            f.SetPosition(0, -0.5f, 0);
+            f.SetScale(50, 1, 50);
+            f.IsShadowCaster = true;
+            AddGameObject(f);
+
+            LightObject sun = new LightObject(LightType.Sun, ShadowQuality.High);
+            sun.SetPosition(-50, 50, 50);
+            sun.SetColor(1, 1, 1, 2);
+            sun.SetNearFar(10, 200);
+            sun.SetFOV(100);
+            AddLightObject(sun);
+        }
+    }
+}
