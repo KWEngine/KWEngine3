0f5cbcf8db3f7ec8c0472b326a14f6d519a46ae2
Lutz Karau <lutz.karau@gmail.com>
2024-03-11 15:00:51 +0100

    working on new test world for mesh pre-rotation...

--- KWEngine3TestProject/Worlds/GameWorldMeshPreRotationTest.cs
+++ KWEngine3TestProject/Worlds/GameWorldMeshPreRotationTest.cs
@@ -1,12 +1,11 @@
-﻿using KWEngine3;
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using KWEngine3;
 using KWEngine3TestProject.Classes;
 using KWEngine3TestProject.Classes.WorldGLTFTest;
 using KWEngine3.Helper;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using KWEngine3.GameObjects;
 
 namespace KWEngine3TestProject.Worlds
@@ -20,24 +19,51 @@ namespace KWEngine3TestProject.Worlds
 
         public override void Prepare()
         {
-            SetCameraPosition(2f, 1f, 2f);
-            SetCameraTarget(0f, 0f, 0f);
-            SetColorAmbient(0.75f, 0.75f, 0.75f);
-            SetBackgroundFillColor(1f, 1f, 1f);
+            KWEngine.LoadModel("Scooter", "./Models/GLTFTest/Vespa.glb");
 
-            KWEngine.LoadModel("Scooter", "./Models/GLTFTest/ScooterKAR.glb");
+            SetCameraPosition(5f, 5f, 5f);
+            SetCameraTarget(0f, 0f, 0f);
+            SetColorAmbient(0.25f, 0.25f, 0.25f);
+            SetBackgroundSkybox("./Textures/skybox.dds");
+            SetBackgroundBrightnessMultiplier(4);
+
+            LightObject sun = new LightObject(LightType.Sun, ShadowQuality.High);
+            sun.Name = "Sun";
+            sun.SetPosition(100, 100, 100);
+            sun.SetNearFar(100, 300);
+            sun.SetFOV(200);
+            sun.SetColor(1, 1, 1, 2.5f);
+            AddLightObject(sun);
+
+            Floor f = new Floor();
+            f.SetScale(100, 1, 100);
+            f.SetPosition(0, -0.5f, 0);
+            f.IsShadowCaster = true;
+            f.IsCollisionObject = true;
+            f.SetTextureRepeat(25, 25);
+            f.SetTexture("./Textures/sand_diffuse.dds", TextureType.Albedo);
+            f.SetTexture("./Textures/sand_normal.dds", TextureType.Normal);
+            AddGameObject(f);
 
             Scooter s = new Scooter();
             s.SetModel("Scooter");
+            s.IsShadowCaster = true;
+            s.SetPosition(0, 0, 0);
+            s.IsCollisionObject = true;
+            s.SetRotation(0, 180, 0);
+            s.UpdateLast = true;
             AddGameObject(s);
         }
     }
 
     internal class Scooter : GameObject
     {
+        private float _degrees = 0f;
+        private float _degreesHandleBar = 0f;
+        private float _velocity = 0f;
         public override void Act()
         {
-            HelperRotation.AddMeshPreRotationX(this, 2, 0.5f);
+            //HelperRotation.AddMeshPreRotationY(this, 1, 0.5f);
             if(Keyboard.IsKeyPressed(OpenTK.Windowing.GraphicsLibraryFramework.Keys.F1))
             {
                 SetModel("KWCube");
@@ -46,6 +72,48 @@ namespace KWEngine3TestProject.Worlds
             {
                 SetModel("Scooter");
             }
+
+            if (Keyboard.IsKeyDown(Keys.W))
+            {
+                if(_velocity == 0f)
+                {
+                    _velocity += 0.0001f;
+                }
+                else
+                {
+                    _velocity = MathHelper.Clamp(_velocity * 1.0075f, 0f, 0.03f);
+                }
+            }
+            else if(Keyboard.IsKeyDown(Keys.S))
+            {
+                _velocity = MathHelper.Clamp(_velocity - 0.001f, 0f, 0.2f);
+            }
+
+            if(Keyboard.IsKeyDown(Keys.A))
+            {
+                _degrees = _degrees + 0.01f;
+                _degreesHandleBar = MathHelper.Clamp(_degreesHandleBar + 0.01f, -10, 10);
+            }
+            if (Keyboard.IsKeyDown(Keys.D))
+            {
+                _degrees = _degrees - 0.01f;
+                _degreesHandleBar = MathHelper.Clamp(_degreesHandleBar - 0.01f, -10, 10);
+            }
+            SetRotation(0, _degrees, 0);
+            HelperRotation.SetMeshPreRotationYZX(this, 1, 0, _degreesHandleBar, 0);
+            MoveAlongVector(LookAtVector, _velocity);
+
+
+            CurrentWorld.SetCameraPosition(this.Position.X - this.LookAtVector.X * 2, 2.5f, this.Position.Z - this.LookAtVector.Z * 2);
+            CurrentWorld.SetCameraTarget(this.Position.X, this.Position.Y, this.Position.Z);
+        }
+    }
+
+    internal class Floor : GameObject
+    {
+        public override void Act()
+        {
+
         }
     }
 }
