78664f6e253afa0208050475691f415b1cfd54c0
Lutz Karau <lutz.karau@gmail.com>
2024-05-07 13:28:16 +0200

    more comments

--- KWEngine3TestProject/Classes/WorldPlaneMTVCollider/Player.cs
+++ KWEngine3TestProject/Classes/WorldPlaneMTVCollider/Player.cs
@@ -137,16 +137,30 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
             // Die Kollisionsprüfung wird ausschließlich für Kollisionsobjekte des Typs Box und 
             // dem Hitboxmodus "Convex Hull" getätigt.
             List<Intersection> intersections = GetIntersections<Box>(IntersectionTestMode.CheckConvexHullsOnly);
+
+            // Dann wird durch die Liste aller entdeckten Kollisionen iteriert:
             foreach(Intersection i in intersections)
             {
+                // Die Spielfigur bewegt sich gemäß der gemessenen Kollision, um die Kollision
+                // ungeschehen zu machen:
                 MoveOffset(i.MTV);
 
+                // Sonderfall! Die Spielfigur kollidiert mit etwas, während sie in der Luft ist:
                 if (_currentState == State.InAir)
                 {
-                    if (Vector3.Dot(Vector3.NormalizeFast(i.MTV), -KWEngine.WorldUp) > 0.1f)
+                    // Wenn der Korrekturvektor (MTV) und die invertierte Welt-Y-Achse 
+                    // ein negatives Skalarprodukt (dot product) haben, dann zeigt
+                    // der Korrekturvektor nach unten. Das bedeutet, dass die Spielfigur
+                    // ein Objekt von unten berührt. In diesem Fall muss die Aufwärts-
+                    // geschwindigkeit (_velocityY) zurückgesetzt werden:
+                    if (Vector3.Dot(Vector3.NormalizeFast(i.MTV), KWEngine.WorldUp) < 0f)
                     {
-                        _velocityY = -0.005f;
+                        _velocityY = 0f;
                     }
+                    
+                    // Sind die XZ-Komponenten des Korrekturvektors != 0, so ist die Spielfigur
+                    // scheinbar gegen eine Wand geprallt. In diesem Fall muss die Bewegungs-
+                    // geschwindigkeit der Spielfigur auf der XZ-Achse auf (0|0) zurückgesetzt werden.
                     if(i.MTV.Xz.LengthSquared > 0)
                     {
                         _velocityXZ = Vector2.Zero;
@@ -155,75 +169,10 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
             }
         }
 
-        private void HandleAnimation()
-        {
-            if(HasAnimations)
-            {
-                if(_animationNeedsToChange)
-                {
-                    _animationStep = 0f;
-                    SetAnimationPercentage(_animationStep);
-                }
-                if(_currentState == State.OnGround)
-                {
-                    if(_velocityXZ.LengthFast > 0.001f)
-                    {
-                        SetAnimationID(11); // Walk
-                        SetAnimationPercentageAdvance(0.005f);
-                    }
-                    else
-                    {
-                        SetAnimationID(3); // Idle
-                        SetAnimationPercentageAdvance(0.001f);
-                    }
-                }
-                else
-                {
-                    if (_animationNeedsToChange)
-                    {
-                        SetAnimationID(6);
-                        _animationStep = 0.10f;
-                        _animationNeedsToChange = false;
-                    }
-                    
-                    if(AnimationID == 6)
-                    {
-                        SetAnimationPercentage(_animationStep);
-                        if (_animationStep >= 1)
-                        {
-                            SetAnimationID(7);
-                            _animationStep = 0f;
-                            SetAnimationPercentage(_animationStep);
-                        }
-                        else
-                        {
-                            _animationStep += 0.006f;
-                        }
-                    }
-                    else if(AnimationID == 7)
-                    {
-                        SetAnimationPercentageAdvance(0.01f);
-                    }
-                }
-                _animationNeedsToChange = false;
-            }
-        }
-
-        private void HandlePlayerFallOff()
-        {
-
-            if (Position.Y < -20)
-            {
-                SetPosition(PLAYER_START);
-                _velocityY = 0f;
-                _velocityXZ = Vector2.Zero;
-                _currentState = State.OnGround;
-                _animationNeedsToChange = true;
-            }
-        }
-
         private Vector2 HandleMovement()
         {
+            // Hier wird gemessen, wieviel zusätzliche Geschwindigkeit
+            // die Spielfigur durch die aktuell gedrückten Tasten erhält:
             Vector2 inputVelocity = Vector2.Zero;
             if (Keyboard.IsKeyDown(Keys.A))
             {
@@ -241,15 +190,24 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
             {
                 inputVelocity.Y += 1f;
             }
+
+            // Hier wird ermittelt, ob die Eingabegeschwindigkeit überhaupt > 0 ist:
             float inputVelocityLengthSq = inputVelocity.LengthSquared;
             if (inputVelocityLengthSq > 0)
             {
+                // Wenn sie >0 ist, dann wird die inputVelocity normalisiert (das bedeutet,
+                // dass die Länge dieses Vektors auf 1 gekürzt/verstärkt wird):
                 inputVelocity.NormalizeFast();
             }
 
+            // Ist die Leertaste oder die rechte Maustaste gedrückt während sich der Spieler 
+            // am Boden befindet, so wird der Status in "in air" gewechselt:
             if ((Keyboard.IsKeyPressed(Keys.Space) || Mouse.IsButtonPressed(MouseButton.Right)) && _currentState == State.OnGround)
             {
                 _currentState = State.InAir;
+
+                // Erhöhe die Aufwärtsgeschwindigkeit um die für die Spielfigur
+                // festgelegte Menge:
                 _velocityY += VELOCITY_JUMP;
                 _animationNeedsToChange = true;
             }
@@ -259,7 +217,20 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
 
         private void ApplyVelocity(Vector2 inputVelocity, Vector3 currentSlopeNormal)
         {
+            // Um zu bestimmen, was die letztendliche Bewegungsgeschwindigkeit für
+            // den Frame ist, müssen die Eingabegeschwindigkeit (inputVelocity) und die
+            // Lage der aktuellen Oberfläche (currentSlopeNormal) betrachtet werden.
+            // Zeigt die Eingaberichtung z.B. an, dass die Spielfigur nach rechts laufen will
+            // aber die aktuelle Oberfläche ist nach links geneigt, soll es für die Spielfigur
+            // schwerer sein, sich auf dieser Oberfläche zu bewegen.
+            
+            // Als erstes berechnen wir, ob die durch die Tasten neu hinzugekommene 
+            // Bewegungsrichtung mit der aktuellen Bewegung übereinstimmt.
+            // Das Skalarprodukt (dot product) ist größer 0, wenn die Richtungen grob übereinstimmen.
+            // Es ist kleiner 0, wenn die Richtungen (ansatzweise) entgegengesetzt sind.
             float dotVelocityInputVelocity = Vector2.Dot(inputVelocity, _velocityXZ);
+
+
             float dotVelocitySlope = Math.Min(0, Vector3.Dot(Vector3.NormalizeFast(new Vector3(_velocityXZ.X, 0, _velocityXZ.Y)), currentSlopeNormal));
             if (dotVelocityInputVelocity >= 0)
             {
@@ -282,6 +253,73 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
             MoveOffset(new Vector3(_velocityXZ.X, _velocityY, _velocityXZ.Y));
 
             _velocityXZ /= VELOCITY_REDUCTION;
+        }
+
+        private void HandleAnimation()
+        {
+            if (HasAnimations)
+            {
+                if (_animationNeedsToChange)
+                {
+                    _animationStep = 0f;
+                    SetAnimationPercentage(_animationStep);
+                }
+                if (_currentState == State.OnGround)
+                {
+                    if (_velocityXZ.LengthFast > 0.001f)
+                    {
+                        SetAnimationID(11); // Walk
+                        SetAnimationPercentageAdvance(0.005f);
+                    }
+                    else
+                    {
+                        SetAnimationID(3); // Idle
+                        SetAnimationPercentageAdvance(0.001f);
+                    }
+                }
+                else
+                {
+                    if (_animationNeedsToChange)
+                    {
+                        SetAnimationID(6);
+                        _animationStep = 0.10f;
+                        _animationNeedsToChange = false;
+                    }
+
+                    if (AnimationID == 6)
+                    {
+                        SetAnimationPercentage(_animationStep);
+                        if (_animationStep >= 1)
+                        {
+                            SetAnimationID(7);
+                            _animationStep = 0f;
+                            SetAnimationPercentage(_animationStep);
+                        }
+                        else
+                        {
+                            _animationStep += 0.006f;
+                        }
+                    }
+                    else if (AnimationID == 7)
+                    {
+                        SetAnimationPercentageAdvance(0.01f);
+                    }
+                }
+                _animationNeedsToChange = false;
+            }
+        }
+
+        private void HandlePlayerFallOff()
+        {
+
+            if (Position.Y < -20)
+            {
+                SetPosition(PLAYER_START);
+                _velocityY = 0f;
+                _velocityXZ = Vector2.Zero;
+                _currentState = State.OnGround;
+                _animationNeedsToChange = true;
+            }
         }
     }
 }
