761a5aaed9338b9cb8a7d2c25b035dba98da24d9
Lutz Karau <lutz.karau@ers-hameln.de>
2024-05-07 06:51:02 +0200

    began commenting on player class for tutorial

--- KWEngine3TestProject/Classes/WorldPlaneMTVCollider/Player.cs
+++ KWEngine3TestProject/Classes/WorldPlaneMTVCollider/Player.cs
@@ -10,76 +10,220 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
 {
     public class Player : GameObject
     {
+        // Aufzählungsdatentypen:
         private enum State
         {
             OnGround,
             InAir
         }
 
+        // Konstanten:
+        private const float VELOCITY_JUMP = 0.05f;
+        private const float VELOCITY_REDUCTION = 1.033f;
+        private const float GRAVITY = 0.0005f;
+        public static readonly Vector3 PLAYER_START = new Vector3(0f, 0f, 0f);
+
+        // Für die Steuerung der Spielfigur benötigte Eigenschaften:
         private Vector2 _velocityXZ = Vector2.Zero;
         private float _velocityY = 0f;
-        private Vector2 _velocitySlopeJump = Vector2.Zero;
         private float _playerSpeed = 0.02f;
-        private const float _velocityReduction = 1.033f;
-        private float _animationStep = 0f;
-
-        private const float VELOCITY_JUMP = 0.04f;
-        private const float GRAVITY = 0.0005f;
-        private State _currentState = State.OnGround;
-        private bool _jumpingUp = false;
         
+        private State _currentState = State.OnGround;
+
+        //Für die Animation der Spielfigur benötigte Eigenschaften:
+        private float _animationStep = 0f;
+        private bool _animationNeedsToChange = false;
 
         public override void Act()
         {
-            bool animationIsChanged = false;
+            // Prüfe, ob Kontakt zum Boden besteht und setze
+            // _currentState dementsprechend auf den korrekten
+            // Wert:
+            Vector3 currentSlopeNormal = HandleGroundDetection();
 
+            // Prüfe, welche Steuerungstasten gedrückt wurden und
+            // bestimme so die durch die Eingabetasten entstehende
+            // Bewegung:
+            Vector2 inputVelocity = HandleMovement();
+
+            // Wende die durch die Eingabe entstandene Geschwindigkeit
+            // auf die aktuell für die Spielfigur gültige 
+            // Geschwindigkeit an:
+            ApplyVelocity(inputVelocity, currentSlopeNormal);
+
+            // Prüfe, ob interaktive Objekte (Gegenstände, bewegliche
+            // Plattformen, usw.) berührt werden:
+            HandleIntersectionsWithObjects();
+            
+            // Passe die Animationen der Spielfigur dem aktuellen
+            // Status (_currentState) an:
+            HandleAnimation();
+
+            // Falls die Spielfigur vom Rand der Welt fällt, 
+            // setze sie wieder zurück auf den Ausgangspunkt:
+            HandlePlayerFallOff();
+        }
+
+        private Vector3 HandleGroundDetection()
+        {
+            // Gehe zunächst von einer ebenen Fläche
+            // (deren Ebenenvektor gerade nach oben zeigt) aus:
             Vector3 slopeNormal = Vector3.UnitY;
+
+            // Ist die Spielfigur gerade im Zustand "am Boden"...
             if (_currentState == State.OnGround)
             {
+                // ...prüfe, ob sich Objekte des Typs Immovable oder Box weniger als 0.1 Einheiten unterhalb 
+                // der Spielfigur befinden:
                 RayIntersectionExtSet set = RaytraceObjectsBelowPosition(RayMode.FourRaysY, 0.75f, -1.0f, 0.1f, typeof(Immovable), typeof(Box));
+
+                // Enthält die Ergebnismenge (ein Set = Menge an Ergebnissen) valide Daten,
+                // wurde mindestens ein Objekt gefunden, das so nah unterhalb des Players liegt:
                 if (set.IsValid)
                 {
+                    // Überschreibe die Steigung der aktuellen Ebene mit der gemessenen Steigung:
                     slopeNormal = set.SurfaceNormalAvg;
+
+                    // Setze die Spielfigur direkt auf den Punkt, der von ihrer Mitte aus unterhalb 
+                    // gemessen wurde:
                     this.SetPositionY(set.IntersectionPointCenter.Y, PositionMode.BottomOfAABBHitbox);
                 }
                 else
                 {
+                    // Wenn die Ergebnismenge kein valides Ergebnis hat, dann wechsele den
+                    // aktuellen Zustand auf "in der Luft", weil die Spielfigur dann nämlich
+                    // in der Luft ist:
                     _currentState = State.InAir;
-                    animationIsChanged = true;
+                    _animationNeedsToChange = true;
                 }
             }
             else
             {
-                if(_jumpingUp)
-                {
-                    if(_velocityY < VELOCITY_JUMP)
-                    {
-                        _velocityY *= 1.1f;
-                    }
-                    else
-                    {
-                        _jumpingUp = false;
-                    }
-                }
-                _velocityY -= GRAVITY;
+                // Ist die Spielfigur in der Luft, wird von ihrer aktuellen 
+                // vertikalen Geschwindigkeit immer wieder ein Stück der aktuellen
+                // Gravitation abgezogen, so dass sich die Fallgeschwindigkeit pro
+                // Frame erhöht. In diesem Beispiel wird die Fallhöhe dann aber auf
+                // -0.5f begrenzt, damit die Spielfigur nicht zu schnell fällt:
+                _velocityY = _velocityY - GRAVITY;
                 if (_velocityY < -0.5f)
                     _velocityY = -0.5f;
 
-                RayIntersectionExtSet set = RaytraceObjectsBelowPosition(RayMode.FourRaysY, 0.75f, -0.5f, 0.05f, typeof(Immovable), typeof(Box));
+                // Auch in der Luft muss geprüft werden, ob sich ein Bodenobjekt nahe den Füßen
+                // der Spielfigur befindet. Hier muss ein Objekt sich aber näher als 0.05f Einheiten
+                // unterhalb der Figur befinden, damit die Figur dies als gültigen Boden erkennt:
+                RayIntersectionExtSet set = RaytraceObjectsBelowPosition(RayMode.FourRaysY, 0.75f, -0.5f, 0.001f, typeof(Immovable), typeof(Box));
+
+                // Wurde ein Boden gefunden, ist die Ergebnismenge "valide":
                 if (set.IsValid == true)
                 {
-                    if (set.DistanceAvg < 0.0001f &&  _velocityY <= 0)
+                    // Ist dann die vertikale Geschwindigkeit auch noch kleiner/gleich 0
+                    // kann die Spielfigur "landen". Würden wir hier nicht auf <= 0 prüfen
+                    // würde die Spielfigur sofort ihre Aufwärtsbewegung stoppen, wenn sie 
+                    // eine bodenähnliche Oberfläche streift:
+                    if (_velocityY <= 0)
                     {
                         _currentState = State.OnGround;
                         this.SetPositionY(set.IntersectionPointCenter.Y, PositionMode.BottomOfAABBHitbox);
-                        animationIsChanged = true;
+                        _animationNeedsToChange = true;
                         _velocityY = 0f;
-                        _jumpingUp = false;
-                        _velocitySlopeJump = Vector2.Zero;
                     }
                 }
             }
+            return slopeNormal;
+        }
 
+        private void HandleIntersectionsWithObjects()
+        {
+            // Die Kollisionsprüfung wird ausschließlich für Kollisionsobjekte des Typs Box und 
+            // dem Hitboxmodus "Convex Hull" getätigt.
+            List<Intersection> intersections = GetIntersections<Box>(IntersectionTestMode.CheckConvexHullsOnly);
+            foreach(Intersection i in intersections)
+            {
+                MoveOffset(i.MTV);
+
+                if (_currentState == State.InAir)
+                {
+                    if (Vector3.Dot(Vector3.NormalizeFast(i.MTV), -KWEngine.WorldUp) > 0.1f)
+                    {
+                        _velocityY = -0.005f;
+                    }
+                    if(i.MTV.Xz.LengthSquared > 0)
+                    {
+                        _velocityXZ = Vector2.Zero;
+                    }
+                }
+            }
+        }
+
+        private void HandleAnimation()
+        {
+            if(HasAnimations)
+            {
+                if(_animationNeedsToChange)
+                {
+                    _animationStep = 0f;
+                    SetAnimationPercentage(_animationStep);
+                }
+                if(_currentState == State.OnGround)
+                {
+                    if(_velocityXZ.LengthFast > 0.001f)
+                    {
+                        SetAnimationID(11); // Walk
+                        SetAnimationPercentageAdvance(0.005f);
+                    }
+                    else
+                    {
+                        SetAnimationID(3); // Idle
+                        SetAnimationPercentageAdvance(0.001f);
+                    }
+                }
+                else
+                {
+                    if (_animationNeedsToChange)
+                    {
+                        SetAnimationID(6);
+                        _animationStep = 0.10f;
+                        _animationNeedsToChange = false;
+                    }
+                    
+                    if(AnimationID == 6)
+                    {
+                        SetAnimationPercentage(_animationStep);
+                        if (_animationStep >= 1)
+                        {
+                            SetAnimationID(7);
+                            _animationStep = 0f;
+                            SetAnimationPercentage(_animationStep);
+                        }
+                        else
+                        {
+                            _animationStep += 0.006f;
+                        }
+                    }
+                    else if(AnimationID == 7)
+                    {
+                        SetAnimationPercentageAdvance(0.01f);
+                    }
+                }
+                _animationNeedsToChange = false;
+            }
+        }
+
+        private void HandlePlayerFallOff()
+        {
+
+            if (Position.Y < -20)
+            {
+                SetPosition(PLAYER_START);
+                _velocityY = 0f;
+                _velocityXZ = Vector2.Zero;
+                _currentState = State.OnGround;
+                _animationNeedsToChange = true;
+            }
+        }
+
+        private Vector2 HandleMovement()
+        {
             Vector2 inputVelocity = Vector2.Zero;
             if (Keyboard.IsKeyDown(Keys.A))
             {
@@ -102,8 +246,21 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
             {
                 inputVelocity.NormalizeFast();
             }
+
+            if ((Keyboard.IsKeyPressed(Keys.Space) || Mouse.IsButtonPressed(MouseButton.Right)) && _currentState == State.OnGround)
+            {
+                _currentState = State.InAir;
+                _velocityY += VELOCITY_JUMP;
+                _animationNeedsToChange = true;
+            }
+
+            return inputVelocity;
+        }
+
+        private void ApplyVelocity(Vector2 inputVelocity, Vector3 currentSlopeNormal)
+        {
             float dotVelocityInputVelocity = Vector2.Dot(inputVelocity, _velocityXZ);
-            float dotVelocitySlope = Math.Min(0, Vector3.Dot(Vector3.NormalizeFast(new Vector3(_velocityXZ.X, 0, _velocityXZ.Y)), slopeNormal));
+            float dotVelocitySlope = Math.Min(0, Vector3.Dot(Vector3.NormalizeFast(new Vector3(_velocityXZ.X, 0, _velocityXZ.Y)), currentSlopeNormal));
             if (dotVelocityInputVelocity >= 0)
             {
                 _velocityXZ += inputVelocity * 0.0005f + inputVelocity * dotVelocitySlope * 0.00045f;
@@ -113,123 +270,18 @@ namespace KWEngine3TestProject.Classes.WorldPlaneMTVCollider
                 _velocityXZ += inputVelocity * 0.0001f;
             }
 
-            
-
-            if ((Keyboard.IsKeyPressed(Keys.Space) || Mouse.IsButtonPressed(MouseButton.Right)) && _currentState == State.OnGround)
-            {
-                _currentState = State.InAir;
-                _jumpingUp = true;
-                _velocityY += VELOCITY_JUMP * 0.33f;
-                _velocitySlopeJump = new Vector2(slopeNormal.X * 0.001f, slopeNormal.Z * 0.001f);
-                animationIsChanged = true;
-            }
-
-            _velocityXZ += _velocitySlopeJump;
-
             float velocitySum = _velocityXZ.LengthFast;
             if (velocitySum > _playerSpeed)
             {
                 _velocityXZ = Vector2.NormalizeFast(_velocityXZ) * _playerSpeed;
             }
-            if (inputVelocityLengthSq > 0)
+            if (inputVelocity.LengthSquared > 0)
             {
                 TurnTowardsXZ(this.Position + new Vector3(_velocityXZ.X, 0, _velocityXZ.Y));
             }
             MoveOffset(new Vector3(_velocityXZ.X, _velocityY, _velocityXZ.Y));
 
-            _velocityXZ /= _velocityReduction;
-            _velocitySlopeJump /= _velocityReduction;
-
-            HandleConvexHulls();
-
-            if (Position.Y < -20)
-            {
-                SetPosition(0, 0.0f, 0);
-                _jumpingUp = false;
-                _velocityY = 0f;
-                _velocityXZ = Vector2.Zero;
-                _velocitySlopeJump = Vector2.Zero;
-                _currentState = State.OnGround;
-                animationIsChanged = true;
-            }
-
-            UpdateAnimation(animationIsChanged);
+            _velocityXZ /= VELOCITY_REDUCTION;
         }
-
-        private void HandleConvexHulls()
-        {
-            List<Intersection> intersections = GetIntersections<Box>(IntersectionTestMode.CheckConvexHullsOnly);
-            foreach(Intersection i in intersections)
-            {
-                MoveOffset(i.MTV);
-
-                if (_currentState == State.InAir)
-                {
-                    if (Vector3.Dot(Vector3.NormalizeFast(i.MTV), -KWEngine.WorldUp) > 0.1f)
-                    {
-                        _velocityY = -0.005f;
-                        _jumpingUp = false;
-                    }
-                    if(i.MTV.Xz.LengthSquared > 0)
-                    {
-                        _velocityXZ = Vector2.Zero;
-                    }
-                }
-            }
-        }
-
-        private void UpdateAnimation(bool resetPercentage)
-        {
-
-            if(HasAnimations)
-            {
-                if(resetPercentage)
-                {
-                    _animationStep = 0f;
-                    SetAnimationPercentage(_animationStep);
-                }
-                if(_currentState == State.OnGround)
-                {
-                    if(_velocityXZ.LengthFast > 0.001f)
-                    {
-                        SetAnimationID(11); // Walk
-                        SetAnimationPercentageAdvance(0.005f);
-                    }
-                    else
-                    {
-                        SetAnimationID(3); // Idle
-                        SetAnimationPercentageAdvance(0.001f);
-                    }
-                }
-                else
-                {
-                    if (resetPercentage)
-                    {
-                        SetAnimationID(6);
-                        _animationStep = 0.10f;
-                    }
-                    
-                    if(AnimationID == 6)
-                    {
-                        SetAnimationPercentage(_animationStep);
-                        if (_animationStep >= 1)
-                        {
-                            SetAnimationID(7);
-                            _animationStep = 0f;
-                            SetAnimationPercentage(_animationStep);
-                        }
-                        else
-                        {
-                            _animationStep += 0.006f;
-                        }
-                    }
-                    else if(AnimationID == 7)
-                    {
-                        SetAnimationPercentageAdvance(0.01f);
-                    }
-                }
-            }
-        }
-
     }
 }
--- KWEngine3TestProject/Worlds/GameWorldPlaneMTVTest.cs
+++ KWEngine3TestProject/Worlds/GameWorldPlaneMTVTest.cs
@@ -12,135 +12,7 @@ namespace KWEngine3TestProject.Worlds
         
         public override void Act()
         {
-            IndexSphere s = GetGameObjectByName<IndexSphere>("IndexSphere");
-            if (s != null)
-            {
-                
-                List<RayIntersectionExt> list = HelperIntersection.RayTraceObjectsForViewVector(HelperIntersection.GetMouseOrigin(), HelperIntersection.GetMouseRay(), 0, true, null, typeof(Immovable));
-                if(list.Count > 0)
-                {
-                    s.SetPosition(list[0].IntersectionPoint);
-                }
-            }
-
-            /*
-            if (Keyboard.IsKeyPressed(Keys.F1))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("Plane");
-                if (i != null)
-                {
-                    Console.WriteLine("Setting collider to custom 'Plane'");
-                    i.SetColliderModel("Plane");
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F2))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("Plane");
-                if (i != null)
-                {
-                    Console.WriteLine("Setting collider back to default model collider");
-                    i.ResetColliderModel();
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F3))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("Plane");
-                if (i != null)
-                {
-                    Console.WriteLine("Setting hitbox 0 to capsule hitbox for Plane object");
-                    i.SetHitboxToCapsule(1, 1, 1, Vector3.Zero, CapsuleHitboxType.Sloped);
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F4))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("Plane");
-                if (i != null)
-                {
-                    Console.WriteLine("ACTIVATING COLLISION: adding hitboxes for Plane object");
-                    i.IsCollisionObject = true;
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F5))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("Plane");
-                if (i != null)
-                {
-                    Console.WriteLine("DEACTIVATING COLLISION: Removing hitboxes for Plane object");
-                    i.IsCollisionObject = false;
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F6))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("PlaneOnTheFly");
-                if (i != null)
-                {
-                    i.ResetColliderModel();
-                    RemoveGameObject(i);
-                }
-                else
-                {
-                    i = new Immovable();
-                    i.Name = "PlaneOnTheFly";
-                    i.SetModel("Plane");
-                    i.IsCollisionObject = true;
-                    i.SetColliderModel("Plane");
-                    i.IsShadowCaster = true;
-                    i.SetPosition(-1, -1, -1);
-                    AddGameObject(i);
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F7))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("PlaneOnTheFly");
-                if (i != null)
-                {
-                    i.IsCollisionObject = false;
-                    RemoveGameObject(i);
-                }
-                else
-                {
-                    i = new Immovable();
-                    i.Name = "PlaneOnTheFly";
-                    i.SetModel("Plane");
-                    i.SetColliderModel("Plane");
-                    i.IsShadowCaster = true;
-                    i.SetPosition(-1, -1, -1);
-                    AddGameObject(i);
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F8))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("PlaneOnTheFly");
-                if (i != null)
-                {
-                    if(i.IsCollisionObject)
-                        Console.WriteLine("Setting PlaneOnTheFly collision state to: deactivated");
-                    else
-                        Console.WriteLine("Setting PlaneOnTheFly collision state to: activated");
-                    i.IsCollisionObject = !i.IsCollisionObject;
-                    
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F9))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("PlaneOnTheFly");
-                if (i != null)
-                {
-                    i.SetModel("KWSphere");
-
-                }
-            }
-            else if (Keyboard.IsKeyPressed(Keys.F10))
-            {
-                Immovable i = GetGameObjectByName<Immovable>("PlaneOnTheFly");
-                if (i != null)
-                {
-                    i.SetModel("Plane");
-                    i.SetColliderModel("Plane");
-
-                }
-            }
-            */
+          
         }
 
         public override void Prepare()
@@ -160,8 +32,7 @@ namespace KWEngine3TestProject.Worlds
             p.IsCollisionObject = true;
             p.IsShadowCaster = true;
             p.SetHitboxToCapsule(true, CapsuleHitboxType.Sloped);
-            //p.SetPosition(0f, 0f, 0f);
-            p.SetPosition(-2.19f, 0.75f, 1.72f);
+            p.SetPosition(Player.PLAYER_START);
             p.SetScale(0.5f);
             p.SetHitboxScale(0.75f, 1.0f, 1.5f);
             AddGameObject(p);
@@ -182,16 +53,6 @@ namespace KWEngine3TestProject.Worlds
             box01.SetColor(1, 0, 1);
             box01.IsShadowCaster = true;
             AddGameObject(box01);
-
-
-            IndexSphere sphere = new IndexSphere();
-            sphere.SetModel("KWSphere");
-            sphere.Name = "IndexSphere";
-            sphere.IsAffectedByLight = false;
-            sphere.SetColorEmissive(1, 0, 0, 2);
-            sphere.SetColor(1, 0, 0);
-            sphere.SetScale(0.25f);
-            AddGameObject(sphere);
 
             LightObject sun = new LightObject(LightType.Sun, ShadowQuality.Low);
             sun.Name = "Sun";
