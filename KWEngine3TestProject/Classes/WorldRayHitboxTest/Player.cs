bb9d59ba96bb5fa0b860a5efa2d069fdcea301ce
Lutz Karau <lutz.karau@ers-hameln.de>
2024-10-30 15:01:27 +0100

    built first test world

---
+++ KWEngine3TestProject/Classes/WorldRayHitboxTest/Player.cs
@@ -0,0 +1,39 @@
+﻿using KWEngine3.GameObjects;
+using KWEngine3.Helper;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KWEngine3TestProject.Classes.WorldRayHitboxTest
+{
+    internal class Player : GameObject
+    {
+        public override void Act()
+        {
+            int forward = 0;
+            int strafe = 0;
+            if (Keyboard.IsKeyDown(Keys.A))
+                strafe -= 1;
+            if (Keyboard.IsKeyDown(Keys.D))
+                strafe += 1;
+            if (Keyboard.IsKeyDown(Keys.W))
+                forward += 1;
+            if (Keyboard.IsKeyDown(Keys.S))
+                forward -= 1;
+
+            CurrentWorld.AddCameraRotationFromMouseDelta();
+            MoveAndStrafeAlongCameraXZ(forward, strafe, 0.01f);
+            CurrentWorld.UpdateCameraPositionForFirstPersonView(Center, 0.5f);
+            TurnTowardsXZ(CurrentWorld.CameraPosition + CurrentWorld.CameraLookAtVector);
+
+            List<RayIntersectionExt> rayHits = HelperIntersection.RayTraceObjectsForViewVector(CurrentWorld.CameraPosition, CurrentWorld.CameraLookAtVector, 0, false, this, typeof(Obstacle));
+            if(rayHits.Count > 0)
+            {
+                Console.WriteLine("HIT @ " + rayHits[0].IntersectionPoint);
+            }
+        }
+    }
+}
---
+++ KWEngine3TestProject/Worlds/GameWorldRayHitboxTest.cs
@@ -0,0 +1,58 @@
+﻿using KWEngine3;
+using KWEngine3.GameObjects;
+using KWEngine3TestProject.Classes.WorldRayHitboxTest;
+
+namespace KWEngine3TestProject.Worlds
+{
+    class GameWorldRayHitboxTest : World
+    {
+        public override void Act()
+        {
+            
+        }
+
+        public override void Prepare()
+        {
+            Player p1 = new Player();
+            p1.AddRotationY(180);
+            AddGameObject(p1);
+
+            // Verbinde die Kamera mit dem Objekt und 
+            // verschiebe sie um 2 Einheiten nach oben: 
+            SetCameraToFirstPersonGameObject(p1, 0.5f);
+            SetCameraFOV(90);
+
+            SetColorAmbient(1, 1, 1);
+            KWEngine.MouseSensitivity = -0.05f;
+
+            // Deaktiviert den Mauszeiger und sorgt dafür, dass sich
+            // der Cursor nicht außerhalb des Programmfensters bewegen kann:
+            MouseCursorGrab();
+
+            // ======================
+
+            Obstacle o1 = new Obstacle();
+            o1.SetColor(1, 0, 1);
+            o1.SetPosition(-5, 0, -10);
+            o1.IsCollisionObject = true;
+            AddGameObject(o1);
+
+            Obstacle o2 = new Obstacle();
+            o2.SetColor(1, 1, 0);
+            o2.SetPosition(0, 0, -10);
+            o2.IsCollisionObject = true;
+            AddGameObject(o2);
+
+            Obstacle o3 = new Obstacle();
+            o3.SetColor(0, 1, 1);
+            o3.SetPosition(+5, 0, -10);
+            o3.IsCollisionObject = true;
+            AddGameObject(o3);
+
+            HUDObjectImage crosshair = new HUDObjectImage("./Textures/crosshair.dds");
+            crosshair.SetScale(8, 8);
+            crosshair.CenterOnScreen();
+            AddHUDObject(crosshair);
+        }
+    }
+}
