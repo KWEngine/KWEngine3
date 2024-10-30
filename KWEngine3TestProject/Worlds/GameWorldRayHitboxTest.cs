374730f16558040acb6643150c761cb12f770a10
Lutz Karau <lutz.karau@ers-hameln.de>
2024-10-30 15:03:32 +0100

    test setup complete

--- KWEngine3TestProject/Worlds/GameWorldRayHitboxTest.cs
+++ KWEngine3TestProject/Worlds/GameWorldRayHitboxTest.cs
@@ -14,7 +14,7 @@ namespace KWEngine3TestProject.Worlds
         public override void Prepare()
         {
             Player p1 = new Player();
-            p1.AddRotationY(180);
+            p1.AddRotationY(205);
             AddGameObject(p1);
 
             // Verbinde die Kamera mit dem Objekt und 
@@ -33,18 +33,21 @@ namespace KWEngine3TestProject.Worlds
 
             Obstacle o1 = new Obstacle();
             o1.SetColor(1, 0, 1);
+            o1.SetScale(1, 2, 1);
             o1.SetPosition(-5, 0, -10);
             o1.IsCollisionObject = true;
             AddGameObject(o1);
 
             Obstacle o2 = new Obstacle();
             o2.SetColor(1, 1, 0);
+            o2.SetScale(1, 2, 1);
             o2.SetPosition(0, 0, -10);
             o2.IsCollisionObject = true;
             AddGameObject(o2);
 
             Obstacle o3 = new Obstacle();
             o3.SetColor(0, 1, 1);
+            o3.SetScale(1, 2, 1);
             o3.SetPosition(+5, 0, -10);
             o3.IsCollisionObject = true;
             AddGameObject(o3);
