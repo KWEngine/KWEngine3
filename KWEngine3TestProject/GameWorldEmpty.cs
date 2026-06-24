90cb8591a89eec2a8906001d4057c2063d397a27
Lutz Karau <lutz.karau@ers-hameln.de>
2026-06-24 13:53:57 +0200

    testing for errors

--- KWEngine3TestProject/GameWorldEmpty.cs
+++ KWEngine3TestProject/GameWorldEmpty.cs
@@ -11,14 +11,34 @@ namespace KWEngine3TestProject
 {
     public class GameWorldEmpty : World
     {
+        TextObject t;
+        float df = 1;
+
         public override void Act()
         {
-            
+            if(Keyboard.IsKeyDown(Keys.Up))
+            {
+                df = Math.Clamp(df + 0.01f, 0.75f, 2f);
+                t.SetCharacterDistanceFactor(df);
+                Console.WriteLine(t.CharacterDistanceFactor);
+            }
+            else if(Keyboard.IsKeyDown(Keys.Down))
+            {
+                df = Math.Clamp(df - 0.01f, 0.75f, 2f);
+                t.SetCharacterDistanceFactor(df);
+                Console.WriteLine(t.CharacterDistanceFactor);
+            }
         }
 
         public override void Prepare()
         {
-            
+            t = new TextObject("MMM");
+            t.SetTextAlignment(TextAlignMode.Center);
+            t.SetPosition(0, 0, 0);
+            t.SetCharacterDistanceFactor(df);
+            t.IsAffectedByLight = false;
+            AddTextObject(t);
+
         }
     }
 }
